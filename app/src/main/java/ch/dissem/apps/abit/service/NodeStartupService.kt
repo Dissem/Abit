6128fd32f9812af4aa28f038d3d4a67480ae0eba
Christian Basler <chrigu.meyer@gmail.com>
2018-08-21 07:39:03 +0200

    👻 Clean up services

--- app/src/main/java/ch/dissem/apps/abit/service/NodeStartupService.kt
+++ app/src/main/java/ch/dissem/apps/abit/service/NodeStartupService.kt
@@ -2,8 +2,17 @@ package ch.dissem.apps.abit.service
 
 import android.app.job.JobParameters
 import android.app.job.JobService
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
+import ch.dissem.apps.abit.notification.NetworkNotification
 import ch.dissem.apps.abit.util.network
 import ch.dissem.apps.abit.util.preferences
+import ch.dissem.bitmessage.BitmessageContext
+import ch.dissem.bitmessage.utils.Property
+import org.jetbrains.anko.doAsync
 
 /**
  * Starts the full node if
@@ -13,12 +22,53 @@ import ch.dissem.apps.abit.util.preferences
  * And stops it when the preconditions for the job (unmetered network) aren't met anymore.
  */
 class NodeStartupService : JobService() {
+    private val bmc: BitmessageContext by lazy { Singleton.getBitmessageContext(this) }
+
+    private lateinit var notification: NetworkNotification
+
+    private val connectivityReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent?) {
+            if (bmc.isRunning() && !preferences.connectionAllowed) {
+                bmc.shutdown()
+            }
+        }
+    }
 
     override fun onStartJob(params: JobParameters?): Boolean {
         if (preferences.online) {
-            network.scheduleNodeStart()
+            registerReceiver(
+                connectivityReceiver,
+                IntentFilter().apply {
+                    addAction(ConnectivityManager.CONNECTIVITY_ACTION)
+                    addAction(Intent.ACTION_BATTERY_CHANGED)
+                }
+            )
+            notification = NetworkNotification(this)
+            NodeStartupService.running = false
+
+            if (!isRunning) {
+                running = true
+                notification.connecting()
+                if (!bmc.isRunning()) {
+                    bmc.startup()
+                }
+                notification.show()
+            }
         }
         return true
+    }
+
+    override fun onDestroy() {
+        if (bmc.isRunning()) {
+            bmc.shutdown()
+        }
+        running = false
+        notification.showShutdown()
+        doAsync {
+            bmc.cleanup()
+        }
+        unregisterReceiver(connectivityReceiver)
+        stopSelf()
     }
 
     /**
@@ -30,4 +80,14 @@ class NodeStartupService : JobService() {
         return false
     }
 
+    companion object {
+        @Volatile
+        private var running = false
+
+        val isRunning: Boolean
+            get() = running && Singleton.bitmessageContext?.isRunning() == true
+
+        val status: Property
+            get() = Singleton.bitmessageContext?.status() ?: Property("bitmessage context")
+    }
 }
--- app/src/main/java/ch/dissem/apps/abit/util/NetworkUtils.kt
+++ app/src/main/java/ch/dissem/apps/abit/util/NetworkUtils.kt
@@ -8,13 +8,16 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import ch.dissem.apps.abit.dialog.FullNodeDialogActivity
-import ch.dissem.apps.abit.service.BitmessageService
+import ch.dissem.apps.abit.service.CleanupService
 import ch.dissem.apps.abit.service.NodeStartupService
 import java.lang.ref.WeakReference
+import java.util.concurrent.TimeUnit
 
 val Context.network get() = NetworkUtils.getInstance(this)
 
 class NetworkUtils internal constructor(private val ctx: Context) {
+
+    private val jobScheduler by lazy { ctx.getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler }
 
     fun enableNode(ask: Boolean = true) {
         if (ask && !ctx.preferences.connectionAllowed) {
@@ -31,24 +34,32 @@ class NetworkUtils internal constructor(private val ctx: Context) {
     }
 
     fun disableNode() {
-        ctx.stopService(Intent(ctx, BitmessageService::class.java))
+        jobScheduler.cancelAll()
     }
 
     fun scheduleNodeStart() {
-        val jobScheduler = ctx.getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
-        val serviceComponent = ComponentName(ctx, NodeStartupService::class.java)
-        val builder = JobInfo.Builder(0, serviceComponent)
-        when {
-            ctx.preferences.wifiOnly ->
-                builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.N ->
-                builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_NOT_ROAMING)
-            else ->
-                builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
+        JobInfo.Builder(0, ComponentName(ctx, NodeStartupService::class.java)).let { builder ->
+            when {
+                ctx.preferences.wifiOnly ->
+                    builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+                Build.VERSION.SDK_INT >= Build.VERSION_CODES.N ->
+                    builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_NOT_ROAMING)
+                else ->
+                    builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
+            }
+            builder.setRequiresCharging(ctx.preferences.requireCharging)
+            builder.setPersisted(true)
+
+            jobScheduler.schedule(builder.build())
         }
-        builder.setRequiresCharging(ctx.preferences.requireCharging)
-        builder.setPersisted(true)
-        jobScheduler.schedule(builder.build())
+
+        JobInfo.Builder(1, ComponentName(ctx, CleanupService::class.java)).let { builder ->
+            builder.setPeriodic(TimeUnit.DAYS.toMillis(1))
+            builder.setRequiresDeviceIdle(true)
+            builder.setRequiresCharging(true)
+
+            jobScheduler.schedule(builder.build())
+        }
     }
 
     companion object {
